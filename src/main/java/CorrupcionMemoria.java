16d96184c6c7928f9262a0b4009d2d45cea2ad92
elias provincia <provinciaelias@gmail.com>
2025-04-27 16:31:44 -0500

    feat: corrupcioon de memoria

---
+++ src/main/java/CorrupcionMemoria.java
@@ -0,0 +1,79 @@
+public class CorrupcionMemoria {
+
+    /**
+     * Este ejemplo muestra como se puede corromper la memoria al no usar
+     * sincronizacion en un objeto compartido entre varios hilos.
+     *
+     * @param args
+     * @throws InterruptedException
+     */
+    public static class Contador{
+        private int valor;
+
+        public void pint(){
+            int old = this.valor;
+            old++;
+            this.valor = old;
+        }
+        public int getValor(){
+            return this.valor;
+        }
+
+    }
+
+    /**
+     * Esta clase es un Runnable que se encarga de llamar al metodo pint del
+     * contador.
+     */
+    public static class Contable implements Runnable{
+        private Contador contador;
+        public  Contable(Contador contador){
+            this.contador = contador;
+        }
+        @Override
+        public void run() {
+            contador.pint();
+        }
+    }
+
+    public static  int HILOS = 1000;
+
+    /**
+     * Este metodo crea 1000 hilos que incrementan el valor de un contador
+     * compartido. Al final se imprime el valor del contador.
+     *
+     * @throws InterruptedException
+     */
+    public static void iterar () throws InterruptedException {
+        Contador c=new Contador();
+        Thread hilos[] = new Thread[HILOS];
+        for (int i = 0; i < HILOS; i++) {
+            hilos[i] = new Thread(new Contable(c));
+        }
+
+        for (int i = 0; i < HILOS; i++) {
+            hilos[i].start();
+        }
+        for (int i = 0; i < HILOS; i++) {
+            hilos[i].join();
+        }
+
+        int total = c.getValor();
+        if (total != HILOS)
+            System.out.println("El total es: " + total);
+
+    }
+
+    public static void main(String[] args) {
+
+        try {
+
+            for (;;){
+                iterar();
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
