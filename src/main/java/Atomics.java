9ab6c1a1192785cd29863d69111b6b3e9c8aa75a
elias provincia <provinciaelias@gmail.com>
2025-05-09 13:31:45 -0500

    init var atomicInteger

---
+++ src/main/java/Atomics.java
@@ -0,0 +1,25 @@
+public class Atomics {
+    static  int total=0;
+    public static class  Counter implements Runnable{
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                total++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Counter());
+        Thread t2 = new Thread(new Counter());
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Total: " + total);
+    }
+}
